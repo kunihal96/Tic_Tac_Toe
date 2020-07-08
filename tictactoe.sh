f6002ab59d04d350060e5a5de128651066ab24d6
kunihal96 <59306190+kunihal96@users.noreply.github.com>
2020-07-08 23:44:57 +0530

    [Badal] Added: Displaying GameBoard

--- tictactoe.sh
+++ tictactoe.sh
@@ -1,13 +1,12 @@
-
 #! /bin/bash
 echo Tic-Tac-Toe problem solved here
 function resetBoard() #reset the board by making all array position as '.'
 {
-boardLength=$1
-for ((i=0;i<$boardLength;i++))
-do
-Board[i]=.
-done
+  boardLength=$1
+  for ((i=0;i<$boardLength;i++))
+  do
+     Board[i]=.
+  done
 }
 
 function toss()
@@ -41,6 +40,28 @@ function toss()
   echo "Computer letter" $computerletter
 }
 
+function displayBoard()
+{
+  echo
+  j=1
+  for ((i=0;i<$boardLength;i++))
+  do
+     if ((j==$boardSize))
+     then
+        echo ' ' ${Board[i]}
+        if ((i!=$boardLength-1))
+        then
+           echo '----- ----- -----'
+        fi
+        j=0
+     else
+        echo -n ' ' ${Board[i]} ' |'
+     fi
+     ((j++))
+  done
+}
+
 boardSize=3
 resetBoard $( expr $boardSize '*' $boardSize)
 toss
+displayBoard
