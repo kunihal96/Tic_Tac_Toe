9fd76d750c21ab88de7091c06b30ce519ee88b6a
kunihal96 <59306190+kunihal96@users.noreply.github.com>
2020-07-08 23:12:15 +0530

    [Badal] Added: Toss For Chance Done Here

--- tictactoe.sh
+++ tictactoe.sh
@@ -1,16 +1,25 @@
 #! /bin/bash
-
-echo Tic Tac Toe game played here
-
+echo Tic-Tac-Toe problem solved here
 function resetBoard() #reset the board by making all array position as '.'
 {
-  boardLength=$1
-
+boardLength=$1
 for ((i=0;i<$boardLength;i++))
 do
-  Board[i]=.
+Board[i]=.
 done
+}
+
+function toss()
+{
+  tossResult=$(( RANDOM % 2 ))
+  if ((tossResult==0))
+  then
+     echo "Player will play first"
+  else
+     echo "Computer will play first"
+  fi
 }
 
 boardSize=3
 resetBoard $( expr $boardSize '*' $boardSize)
+toss
