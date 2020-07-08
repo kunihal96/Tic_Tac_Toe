21b2ce628f0f9f6d56fc2720577b782dc2058acc
kunihal96 <59306190+kunihal96@users.noreply.github.com>
2020-07-08 22:58:36 +0530

    [Badal] Added: Game Board Resetted

--- tictactoe.sh
+++ tictactoe.sh
@@ -2,3 +2,15 @@
 
 echo Tic Tac Toe game played here
 
+function resetBoard() #reset the board by making all array position as '.'
+{
+  boardLength=$1
+
+for ((i=0;i<$boardLength;i++))
+do
+  Board[i]=.
+done
+}
+
+boardSize=3
+resetBoard $( expr $boardSize '*' $boardSize)
